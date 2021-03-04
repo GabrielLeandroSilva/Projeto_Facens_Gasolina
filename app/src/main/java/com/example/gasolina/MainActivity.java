29f6b696b26a28cd9313bc6dda0eba7751a67139
Gabriel <gleandro0795@gmail.com>
2021-03-04 20:28:48 -0300

    Create the project gasoline

---
+++ app/src/main/java/com/example/gasolina/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.gasolina;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void CalcBestPrice(View view) {
+
+        TextInputEditText alcool = findViewById(R.id.textInputEditText);
+        TextInputEditText gasoline = findViewById(R.id.textInputEditText2);
+        TextView result = findViewById(R.id.textView3);
+        
+        if(Float.parseFloat(alcool.getText().toString()) > Float.parseFloat(gasoline.getText().toString())) {
+            result.setText("Resultado: Gasolina é a melhor opção");
+        } else {
+            result.setText("Resultado: Alcool é a melhor opção");
+        }
+
+    }
+}
