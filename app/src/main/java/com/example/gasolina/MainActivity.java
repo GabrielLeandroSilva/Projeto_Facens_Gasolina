7dafdefd096e29d8847072091d91a98e2b36a88c
Gabriel <gleandro0795@gmail.com>
2021-03-05 19:49:16 -0300

    Update the validation in the fields

--- app/src/main/java/com/example/gasolina/MainActivity.java
+++ app/src/main/java/com/example/gasolina/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 
@@ -21,6 +22,17 @@ public class MainActivity extends AppCompatActivity {
         TextInputEditText alcool = findViewById(R.id.textInputEditText);
         TextInputEditText gasoline = findViewById(R.id.textInputEditText2);
         TextView result = findViewById(R.id.textView3);
+
+        if(alcool.getText().toString().equals("")) {
+            Toast.makeText(getApplicationContext(), "Por favor é necessario colocar um valor para o alcool", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(gasoline.getText().toString().equals("")) {
+            Toast.makeText(getApplicationContext(), "Por favor é necessario colocar um valor para a gasolina", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
         
         if(Float.parseFloat(alcool.getText().toString()) <= Float.parseFloat(gasoline.getText().toString()) * 0.7f) {
             result.setText("Resultado: Alcool é a melhor opção");
